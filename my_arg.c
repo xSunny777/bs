774703b214867e2a6223d789ff1ca06bc2845dbe
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2023-10-19 14:37:13 +0200

    Update my_arg.c initial commit my_arg

--- my_arg.c
+++ my_arg.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    int i; 
+
+    for (int i = 0; i < argc; i++)
+    {
+        printf("argv[%d] = %s", i, argv[i]);
+        printf("\n");
+    }
+    return EXIT_SUCCESS;
+}
