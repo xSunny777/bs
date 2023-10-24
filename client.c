598b3e602665b82ffb33c1d044a8cbc8e8d7a94e
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2023-10-24 16:25:13 +0200

    Comments in client

--- client.c
+++ client.c
@@ -1,14 +1,13 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
+#include <netdb.h> //includiert gethostbyname  &  getaddrinfo
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 
-#define BUF_SIZE 1024
-
+#define BUF_SIZE 1024 //Legt größe puffer für daten fest 
 int main(int argc, char *argv[]) {
 
 	int	sock;
@@ -17,49 +16,54 @@ int main(int argc, char *argv[]) {
 	struct hostent *host;
     
 	//Prüfen der eingaben
-	if(argc != 3)
+	if(argc != 3) //Solange es nicht genau 2 Arguments eingegeben werden, kommt diese Meldung (0te Argument)
     {
-		printf("\n%s [Namen des Zielrechners] [Port-Id] \n\n", argv[0]);
+		printf("\n%s [Namen des Zielrechners] [Port-Id] \n\n", argv[0]); 
 		return EXIT_FAILURE;
 	}
 	
-	if(atoi(argv[2]) < 1) //atoi: ASCII to Integer
+    //atoi: ASCII to Integer; Port wird in positive ganzzahl umgewandelt
+    //falls Port negativ, gibt Fehlermeldung
+	if(atoi(argv[2]) < 1) 
     {
-		printf("\n Port Falsch angegeben!\n\n");
+		printf("\n Port Falsch angegeben!\n\n"); //Wenn port ungültig wird Fehlermeldung ausgegeben
 		return EXIT_FAILURE;
 	}
-	port = (unsigned) atoi(argv[2]);
+	port = (unsigned) atoi(argv[2]); //zweite Argument= unsigned int port
 	
-	host = gethostbyname(argv[1]);
+	host = gethostbyname(argv[1]); //Funktion wandelt Hostname in IP Adresse um
 
-	if(host == NULL)    
+	if(host == NULL)    //Falls der Hostname nicht umgewandelt werden kann wird Fehlrmeldung ausgegeben
     {
 		printf("\n Host Falsch angegeben!\n\n");
 		return EXIT_FAILURE;
 	}
 
 	// Socket
-	sock = socket(AF_INET,SOCK_STREAM,0);
+    //stellt Verbindung zum Server her
+	sock = socket(AF_INET,SOCK_STREAM,0); //Stream-Socket, mit IPv4, Protokolltyp 0
+    
+    //Wenn Socket erfolgreich erstellt gibt positive ganze Zahl zurück
 	if(sock < 0)    
     {
 		printf("\n Socket konnte nicht erzeugt werden!\n\n");
 		return EXIT_FAILURE;
 	}
 
-	//sockaddr_in server Füllen
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	bcopy(host->h_addr_list, &server.sin_addr, host->h_length);
+	//Serveradresse wird konfiguriert
+	server.sin_family = AF_INET; //IPv4 Verbindung
+	server.sin_port = htons(port); //benutze Port 
+	bcopy(host->h_addr, &server.sin_addr, host->h_length); //IP-Adresse Zielrechner Client wird in Struktur Server kopiert
 
 	//Verbindung herstellen
 	if(connect(sock,(struct sockaddr*) &server,sizeof(server)) < 0)    
     {
-		printf("\n Verbindung konnte nicht hergestellt werden!\n\n");
+		printf("\n Verbindung konnte nicht hergestellt werden!\n\n"); //Gibt Fehlermeldung wenn fehlschlägt
 		return EXIT_FAILURE;
 	}
 
 	//Senden
-	if( send(sock , "CLIENT" , strlen("CLIENT") , 0) < 0)
+	if(send(sock ,"CLIENT" ,strlen("CLIENT") , 0) < 0)
     {
 		printf("\n Senden Fehlgeschlagen!\n\n");
 		return EXIT_FAILURE;
@@ -67,15 +71,15 @@ int main(int argc, char *argv[]) {
 	printf("\nSenden: CLIENT\n");
 	
 	//Empfangen
-	char antwort[2000];
-	if( recv(sock ,antwort ,2000 ,0) < 0)
+	char antwort[BUF_SIZE]; //Max Größe char
+	if(recv(sock ,antwort ,BUF_SIZE ,0) < 0) //Wenn keine Nachricht ankommt, gibt Fehlermeldung
     {
 		printf("\n Empfangen Fehlgeschlagen!\n\n");
 		return EXIT_FAILURE;
 	}
 	printf("\nAntwort: %s\n",antwort);
 		
-	close(sock);
+	close(sock); //Schließt Verbindung
 
 	return EXIT_SUCCESS;
 }
