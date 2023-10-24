c8afd1a6215d3032dd3a10c210060c73393bdc7a
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2023-10-24 14:02:02 +0200

    changes in code structure

--- my_shell.c
+++ my_shell.c
@@ -6,35 +6,45 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-	
 
 	char string[100];
 	int statval;
-	while(1){
+
+	while(1)
+	{
 		printf("\n");
 		printf("Kommando: ");
 		scanf("%s",string);
-		if(strcmp(string, "q") == 0){
+
+		if(strcmp(string, "c") == 0)
+		{
 			return EXIT_SUCCESS;
 		}
+
 		printf("\n");
 		errno = 0;
 		int id;
-		if((id = fork()) > 0){
-			wait(&statval);
+
+		if((id = fork()) > 0) //Kopie des aktuellen Prozesses --> Kindprozess
+		{
+			wait(&statval); //Vater wartet auf Kind
 			printf("\n");
-            printf("\n");
 			printf("Vater: \n");
-			printf("res: %d errno: %d; status: %d", id, errno, WEXITSTATUS(statval));
-		}else{
+			//0: Prozess erfolgreich abgeschlossen
+			printf("res: %d errno: %d; status: %d", id, errno, WEXITSTATUS(statval)); 
+			printf("\n");
+		}
+
+		else
+		{
 			printf("Sohn: \n");
 			char *args[] = {"",NULL};
 			int s = execv(string, args);
 			printf("res: %d; errno: %d",s, errno);
+			printf("\n");
 			exit(s);
 		}
 	}
-
 
    return EXIT_SUCCESS;
 }
