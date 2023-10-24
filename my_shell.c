5bbc317698fa7b7fe6b58733f4aaea0cbed153fc
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2023-10-24 14:14:04 +0200

    Pfad zu date

--- my_shell.c
+++ my_shell.c
@@ -30,7 +30,8 @@ int main(int argc, char *argv[]) {
 			wait(&statval); //Vater wartet auf Kind
 			printf("\n");
 			printf("Vater: \n");
-			//0: Prozess erfolgreich abgeschlossen
+			//pid, Fehlercode, EXIT-Status
+			//0: Prozess erfolgreich abgeschlossen (0-255)
 			printf("res: %d errno: %d; status: %d", id, errno, WEXITSTATUS(statval)); 
 			printf("\n");
 		}
@@ -38,8 +39,8 @@ int main(int argc, char *argv[]) {
 		else
 		{
 			printf("Sohn: \n");
-			char *args[] = {"",NULL};
-			int s = execv(string, args);
+			char *args[] = {"/bin/date",NULL}; //Pfad zu "date"
+			int s = execv("/bin/date", args); //Befehl ausführen, Argument:/bin/date
 			printf("res: %d; errno: %d",s, errno);
 			printf("\n");
 			exit(s);
