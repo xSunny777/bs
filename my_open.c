f2feb730d3a4a31db75cb4dfe0e9a91747721af0
xSunny777 <hetian.zhang@study.hs-duesseldorf.de>
2023-10-25 12:04:24 +0200

    comments to my_open.c

--- my_open.c
+++ my_open.c
@@ -6,25 +6,40 @@
 #include <unistd.h>
 #include <errno.h>
 
-int main(int argc, char **argv){
-
-	if(argc != 2) {
+int main(int argc, char **argv)
+{
+	//Solange es nicht genau 1 Arguments eingegeben werden, kommt diese Meldung (0te Argument)
+	if(argc != 2) 
+	{
 		printf("Bitte geben Sie die Argumente in dem Format [Dateiname] ein!\n\n");
 		return EXIT_FAILURE;
 	}
+
 	int c;
 	FILE *file;
+	//Lesemodus, öffnet die Datei (Argument 1)
 	file = fopen(argv[1], "r");
-	if (file) {
+
+	//Überprüft, ob das Öffnen der Datei erfolgreich war
+	//file zeigt auf die Dateiname 
+	if (file) 
+	{
 		printf("\n");
-    		while ((c = getc(file)) != EOF){
+			//Solange es noch Zeichen gibt, gibt er die aus
+    		while ((c = getc(file)) != EOF)
+			{
         		putchar(c);	
     		}
+			//file close
     		fclose(file);
     		printf("\n");
-	}else{
-		perror("Fehler bei open ");
-		fprintf(stderr, "%s\n", strerror(errno));
+	}
+	
+	//Fehlermeldung; file zeigt auf NULL
+	else
+	{
+		perror("Fehler bei open "); //Fehlermeldung auf stderr
+		fprintf(stderr, "%s\n", strerror(errno)); ////Fehlernummer wird in der erstellten Datei ausgegeben
 		return EXIT_FAILURE;
 	}
 
